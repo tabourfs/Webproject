6ca0e3bfb292e73f3eea09e9c32d82acd569175b
tabourfs <tabour.fs@mailo.com>
2026-05-19 23:37:46 +0200

    Initial

---
+++ backend/php/psignin.php
@@ -0,0 +1,13 @@
+<?php
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
+$api = $_POST['api'] ?? '';
+
+header("Location: http://localhost:8080/main.html");
+exit;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+</html>
