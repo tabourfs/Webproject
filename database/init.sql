b83eb6571a4bf084323e0e55d3a15e86804acbf1
tabourfs <tabour.fs@mailo.com>
2026-05-21 17:06:34 +0200

    full database

--- database/init.sql
+++ database/init.sql
@@ -1,9 +1,9 @@
--- phpMyAdmin SQL Dump
+- phpMyAdmin SQL Dump
 -- version 5.2.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: db:3306
--- Generation Time: May 20, 2026 at 02:19 PM
+-- Generation Time: May 21, 2026 at 03:05 PM
 -- Server version: 8.0.46
 -- PHP Version: 8.3.26
 
@@ -24,6 +24,55 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `file`
+--
+
+CREATE TABLE `file` (
+  `id` int NOT NULL,
+  `path` varchar(16000) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `folder`
+--
+
+CREATE TABLE `folder` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `path` varchar(16000) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `folder_file`
+--
+
+CREATE TABLE `folder_file` (
+  `id` int NOT NULL,
+  `folder_id` int NOT NULL,
+  `file_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `folder_folder`
+--
+
+CREATE TABLE `folder_folder` (
+  `id` int NOT NULL,
+  `parent_folder_id` int NOT NULL,
+  `child_folder_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -39,6 +88,35 @@ CREATE TABLE `user` (
 --
 
 --
+-- Indexes for table `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `folder`
+--
+ALTER TABLE `folder`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `folder_file`
+--
+ALTER TABLE `folder_file`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `folder_id` (`folder_id`),
+  ADD KEY `file_id` (`file_id`);
+
+--
+-- Indexes for table `folder_folder`
+--
+ALTER TABLE `folder_folder`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `parent_folder_id` (`parent_folder_id`),
+  ADD KEY `child_folder_id` (`child_folder_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -49,10 +127,58 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `file`
+--
+ALTER TABLE `file`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `folder`
+--
+ALTER TABLE `folder`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `folder_file`
+--
+ALTER TABLE `folder_file`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `folder_folder`
+--
+ALTER TABLE `folder_folder`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `folder`
+--
+ALTER TABLE `folder`
+  ADD CONSTRAINT `folder_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `folder_file`
+--
+ALTER TABLE `folder_file`
+  ADD CONSTRAINT `folder_file_ibfk_1` FOREIGN KEY (`folder_id`) REFERENCES `folder` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `folder_file_ibfk_2` FOREIGN KEY (`file_id`) REFERENCES `file` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `folder_folder`
+--
+ALTER TABLE `folder_folder`
+  ADD CONSTRAINT `folder_folder_ibfk_1` FOREIGN KEY (`parent_folder_id`) REFERENCES `folder` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `folder_folder_ibfk_2` FOREIGN KEY (`child_folder_id`) REFERENCES `folder` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
